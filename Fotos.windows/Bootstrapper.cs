621413c288239b18b2cb7fa339e84e352776db31
Cliff <tzcliff@outlook.com>
2020-05-02 22:20:24 -0400

    Dependency Injection

---
+++ Fotos.windows/Bootstrapper.cs
@@ -0,0 +1,60 @@
+﻿using Autofac;
+using Caliburn.Micro;
+using Fotos.Windows.ViewModels;
+using Fotos.Windows.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Fotos.Windows
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container.Singleton<IWindowManager, WindowManager>();
+            _container.Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(ViewModelType =>
+                _container.RegisterPerRequest(ViewModelType, ViewModelType.ToString(), ViewModelType));
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            var instance = _container.GetInstance(service, key);
+            if (instance != null)
+                return instance;
+            throw new InvalidOperationException("Could not locate any instances.");
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
--- Fotos.windows/ViewModels/ShellViewModel.cs
+++ Fotos.windows/ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace Fotos.Windows.ViewModels
 {
-    class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        WelcomeViewModel _welcomeVM;
+
+        public ShellViewModel(WelcomeViewModel welcomeViewModel)
+        {
+            _welcomeVM = welcomeViewModel;
+            ActivateItem(_welcomeVM);
+        }
     }
 }
